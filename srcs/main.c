6887c23dbfef86f578c74be5dddbcbd51c39507e
odgigodji <AZM-snn-J9f-G2L>
2021-02-18 17:48:31 +0300

    violet color

--- srcs/main.c
+++ srcs/main.c
@@ -3,6 +3,11 @@
 int main(int argc, char const **argv, char const **envp)
 {
 //	printf("hello");
-	pre_executer(argc, argv, envp);
+	char *line;
+
+	ft_putstr_fd("\033[35mminishell$ \033[0m", 0);
+	get_next_line(1, &line);
+	printf("%s", line);
+//	pre_executer(argc, argv, envp);
 	return (0);
 }
