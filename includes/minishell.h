6d3e1822f14fdc6afd65e3615007b22acf819464
mscot <zotochev@gmail.com>
2021-02-18 18:29:56 +0300

    Добавил структыры из статьи в minishell.h

--- includes/minishell.h
+++ includes/minishell.h
@@ -1,5 +1,3 @@
-#include <__wctype.h>
-
 #ifndef MINISHELL_H
 # define MINISHELL_H
 
@@ -8,8 +6,45 @@
 # include <unistd.h>
 # include <sys/types.h>
 # include <sys/uio.h>
-
 # include "libft.h"
+
+/*
+**	Command Data structure
+*/
+
+/*
+**	Describe a simple command and arguments
+*/
+
+typedef struct	s_simple_command
+{
+	//	Available space for arguments currently preallocated
+	int		number_of_available_arguments;
+	//	Number of arguments
+	int		number_of_arguments;
+	//	Array of arguments
+	char	**arguments;
+}				t_simple_command;
+
+
+/*
+**	Describe a complete command with the multiple pipes if any
+**	and input/output redirection if any.
+*/
+
+typedef struct	s_command
+{
+	int					number_of_available_simple_commands;
+	int 				number_of_simple_commands;
+	t_simple_command	**simple_commands;
+	char				*out_file;
+	char 				*input_file;
+	char 				*err_file;
+	int 				background;
+
+
+}				t_command;
+
 
 int		get_next_line(int fd, char **line);
 
