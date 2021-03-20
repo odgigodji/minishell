8aedc956a39a0cef700a264a387e6229caa8d418
odgigodji <AZM-snn-J9f-G2L>
2021-03-20 19:02:04 +0300

    new_pars shit сырые пайпы: добавил счетчик аргументов для новой симпл команд

--- srcs/parser/new_pars.c
+++ srcs/parser/new_pars.c
@@ -1,18 +1,21 @@
 #include "minishell.h"
 
-void ft_init_next_simple_command(t_common *common)
+void ft_init_next_simple_command(t_common *common, char *line)
 {
 	int current_command;
 	int arg_count;
 
-//	arg_count = ft_arg_counter(line);	//считаем аргументы симпл команды
+	printf(RED"line after '|' is <%s>\n"RESET, line);
+	arg_count = ft_arg_counter(line);	//считаем аргументы симпл команды
+//	printf("next_arg_count is %d\n", arg_count)
 	printf("->%d\n", arg_count);
 	current_command = common->command.current_simple_command;
 
 	common->command.simple_commands[current_command] = ft_calloc(sizeof(t_simple_command) , 1); //одна команда без пайпов
-	common->command.simple_commands[current_command]->arg_count = 3;//ft_arg_counter(line);//3;   //кол во аргументов для новой симпл команды  fixme
+//	common->command.simple_commands[current_command]->arg_count = 3;//ft_arg_counter(line);//3;   //кол во аргументов для новой симпл команды  fixme
 
-	common->command.simple_commands[current_command]->arguments = ft_calloc(sizeof(char *),3 + 1);
+	common->command.simple_commands[current_command]->arguments = ft_calloc(sizeof(char *),arg_count + 1);
+	common->command.simple_commands[current_command]->arg_count = arg_count;
 	common->command.simple_commands[current_command]->current_arg = 0;
 //	common->command.simple_commands[current_command]->current_arg = 0;
 }
@@ -86,7 +89,7 @@ int do_spec(t_common *common, char *line, char curent_char, int increment)
 		printf("current char is <%c>\n", curent_char);
 		printf("current line is <%s>\n", line);
 		common->command.current_simple_command++;
-		ft_init_next_simple_command(common);
+		ft_init_next_simple_command(common, line + 1);
 	}
 	increment++;
 	return (increment);
