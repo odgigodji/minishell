f2a72a654cc22edc08d2eddc1d88592da6fed899
odgigodji <AZM-snn-J9f-G2L>
2021-03-24 19:54:07 +0300

    new_pars: пока плюс минус чето работает с редиректами редиректы пока отключены

--- srcs/new_parser/do_redirect.c
+++ srcs/new_parser/do_redirect.c
@@ -1,56 +1,56 @@
 #include "minishell.h"
 
-int ft_init_out_or_input_file(t_common *common, char *line, int increment)
+int ft_make_outfile(t_common *common, char *line, int increment, int current_out_file)
+{
+
+	int outfile_len;
+	outfile_len = ft_strlen_to_char(line + increment, ' ');
+	printf(BG_WHT"%s\n"RESET, line + increment);
+	printf("OUTFILE LEN [%d]\n"RESET, (int)outfile_len);
+	printf("CURRENT_OUT_FILE IS %d\n", current_out_file);
+
+	common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file] = ft_calloc(sizeof(char), outfile_len + 1);
+	ft_strlcat(common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file],\
+	line + increment, outfile_len + 1); // +1 на \0
+	printf("current simple command is %d\n", common->command.current_simple_command);
+	printf(RED"[%s]\n"RESET, common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file]);
+	current_out_file++;
+	common->command.simple_commands[common->command.current_simple_command]->arg_count--;
+	return (outfile_len);
+}
+
+int ft_do_outfile(t_common *common, char *line, int increment)
 {
 	static int current_out_file = 0;
 	int num_of_outfiles;
-	int outfile_len;
+	int ret;
 
 	num_of_outfiles = common->command.simple_commands[common->command.current_simple_command]->outfile_count;
-	outfile_len = ft_strlen_to_char(line + increment, ' ');
-	printf(BG_WHT"%s\n"RESET, line + increment);
-	printf("BG_WHT[%d]\n"RESET, (int)outfile_len);
-	printf("CURRENT_OUT_FILE IS %d\n", current_out_file);
-
-
 	if (common->command.simple_commands[common->command.current_simple_command]->have_pipe && current_out_file == num_of_outfiles)
 	{
 		current_out_file = 0;
 		printf("CURRENT_OUT_FILE IS %d(should be 0)\n", current_out_file);
 		common->command.simple_commands[common->command.current_simple_command]->have_pipe = 0;
 
-		common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file] = ft_calloc(sizeof(char), outfile_len + 1);
-		ft_strlcat(common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file],\
-	line + increment, outfile_len + 1); // +1 на \0
-		printf("current simple command is %d\n", common->command.current_simple_command);
-		printf(RED"[%s]\n"RESET, common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file]);
-		current_out_file++;
-		common->command.simple_commands[common->command.current_simple_command]->arg_count--;
+		ret = ft_make_outfile(common, line, increment, current_out_file);
+
 //		common->command.simple_commands[current_command]->out_file[num_of_outfile] = NULL;
 		common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file] = NULL;
-		return (outfile_len);
+		return (ret);
 	}
 	if (current_out_file != num_of_outfiles)
 	{
-		common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file] = ft_calloc(sizeof(char), outfile_len + 1);
-		ft_strlcat(common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file],\
-	line + increment, outfile_len + 1); // +1 на \0
-		printf("current simple command is %d\n", common->command.current_simple_command);
-		printf(RED"[%s]\n"RESET, common->command.simple_commands[common->command.current_simple_command]->out_file[current_out_file]);
-		current_out_file++;
-		common->command.simple_commands[common->command.current_simple_command]->arg_count--;
+		ret = ft_make_outfile(common, line, increment, current_out_file);
 	}
 
-	return (outfile_len);
+	return (ret);
 }
 
 int do_redirect(t_common *common, char *line)
 {
 	int i;
-	int outfile_len;
 
 	i = 0;
-
 //	common->command.simple_commands[common->command.current_simple_command]->out_file[0] = "\0";//
 	if (line[i] == '>')
 	{
@@ -62,14 +62,12 @@ int do_redirect(t_common *common, char *line)
 				i++;
 			else
 			{
-				i += ft_init_out_or_input_file(common, line, i);
+				i += ft_do_outfile(common, line, i);
 				return (i);
 			}
-//			i++;
 		}
 //		printf("----->|%s|\n", common->command.simple_commands[common->command.current_simple_command]->out_file[0]);
 //		common->command.simple_commands[common->command.current_simple_command]->out_file[0];
 	}
-//	printf("RDRCT---\n");
 	return 0;
 }
