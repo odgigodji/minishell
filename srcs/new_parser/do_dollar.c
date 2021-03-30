007d55543f40449732cc616b29e409ab13cb730c
odgigodji <AZM-snn-J9f-G2L>
2021-03-30 19:08:26 +0300

    new_pars: начинаю доллар

--- srcs/new_parser/do_dollar.c
+++ srcs/new_parser/do_dollar.c
@@ -1,22 +1,29 @@
 #include "minishell.h"
 
-void ft_print_env(t_common *common, char **env_variables)
+void ft_print_env(t_common *common, char **env_variables, char ***env_variables_list, char *res)
 {
 	int i = 0;
-
-	while(env_variables[i])
+	int j = 0;
+	char *ret = ft_calloc(sizeof(char), 50);
+	while(env_variables_list[i])
 	{
-		if (DEB_DOLLAR)
-		printf(YEL"|%s|\n"RESET, env_variables[i]);
+		while(env_variables_list[i][j])
+		{
+			if (DEB_DOLLAR)
+				printf(YEL"|%s|\n"RESET, env_variables_list[i][j]);
+			j++;
+		}
 		i++;
 	}
+	ret = get_envp_var_pointer(common, res);
+	printf(RED"|%s|\n"RESET, ret);
 }
 
 
 int ft_do_dollar(t_common *common, char *line)
 {
-
 	printf(GRN"dollar\n"RESET);
-	ft_print_env(common, common->env_variables);
+	char res[10] = "SHELL";
+	ft_print_env(common, common->env_variables, common->env_variables_list, res);
 	return (0);
 }
