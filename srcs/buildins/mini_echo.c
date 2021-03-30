daca81ad61d6ad6aea87751ae1aa1fff9eafc7ca
mscot <zotochev@gmail.com>
2021-03-30 11:57:01 +0300

    добавил пропуск -n ключей для echo.

--- srcs/buildins/mini_echo.c
+++ srcs/buildins/mini_echo.c
@@ -5,17 +5,14 @@ void	mini_echo(char **simple_command)
 	int count_arg;
 
 	count_arg = 1;
-	if (!ft_strncmp(simple_command[count_arg], "-n", 3))
+	while (!ft_strncmp(simple_command[count_arg], "-n", 3))
 		count_arg++;
-	else
+	while (simple_command[count_arg])
 	{
-		while (simple_command[count_arg])
-		{
-			ft_putstr_fd(simple_command[count_arg], STDOUT_FILENO);
-			if (simple_command[count_arg + 1])
-				ft_putchar_fd(' ', STDOUT_FILENO);
-			count_arg++;
-		}
+		ft_putstr_fd(simple_command[count_arg], STDOUT_FILENO);
+		if (simple_command[count_arg + 1])
+			ft_putchar_fd(' ', STDOUT_FILENO);
+		count_arg++;
 	}
 	if (!ft_strncmp(simple_command[1], "-n", 3))
 		return ;
