ae35ecd69ce28e2b2a9dcec4601f9cb8ef924b46
odgigodji <AZM-snn-J9f-G2L>
2021-04-03 20:39:18 +0300

    parser_idea.c : save0.6

--- srcs/parser/parser_idea.c
+++ srcs/parser/parser_idea.c
@@ -92,10 +92,12 @@ t_command get_command_table(char **lexer_result)
 	t_simple_command	**command_table; //simple_commands
 //	t_simple_command	*simple_command; //one simple_command
 	int					counter;
+	int 				last_token;
 
 	counter = 0;
 	int current_token = 0;
 	command.current_token = 0;
+	last_token = ft_array_len(lexer_result);
 	command_table = command_table_init(lexer_result);		//выделить память и занулить
 //	count = 0;
 //	command_table[0] = get_simple_command(lexer_result, &command);
@@ -105,14 +107,15 @@ t_command get_command_table(char **lexer_result)
 //	command_table[2] = get_simple_command(lexer_result + command.current_token, &command);
 //	printf("current token is %d\n", command.current_token);
 
-	while(current_token < ft_array_len(lexer_result) && (command_table[counter] = get_simple_command(lexer_result, &current_token)))
+	while(NOT_LAST_TOKEN) //&& (command_table[counter] = get_simple_command(lexer_result, &current_token)))
 	{
+		command_table[counter] = get_simple_command(lexer_result, &current_token);
 //		command_table[counter] = get_simple_command(lexer_result + current_token, &current_token);
 		ft_print_simple_comand(command_table[counter]);
 		counter++;
 	}
 
-	exit(0);
+//	exit(0);
 //	while ((simple_command = get_simple_command(common, lexer_result)))
 //	{
 ////		exit (0);
