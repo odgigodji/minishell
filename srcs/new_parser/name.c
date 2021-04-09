2bfde1355cde3a758b3cf119fe1a4f518a9e0e83
odgigodji <AZM-snn-J9f-G2L>
2021-04-09 17:35:10 +0300

    parser_idea.c : save4.2 защита кавычек + редиректы +-

--- srcs/new_parser/name.c
+++ srcs/new_parser/name.c
@@ -19,16 +19,34 @@ int with_error(const int unexpected_token)
 {
 	if (unexpected_token == '\"' || unexpected_token == '\'')
 	{
-		printf(RED"syntax error : unclosed quote "RESET);
+		printf(RED"syntax error : unclosed quotes "RESET);
 		printf(BLU"'%c'\n"RESET, unexpected_token);
 		errno = 42;
 	}
 	else
 	{
-		printf(RED"syntax error near unexpected token '%c'\n"RESET, unexpected_token);
+		printf(RED"syntax error near unexpected token "RESET);
+		printf(BLU"'%c'\n"RESET, unexpected_token);
 		errno = 258;
 	}
 	return (1);
+}
+
+char check_line_2(const char *line, int i, int quotes_flag, char quote_type)
+{
+	while(line[++i])
+	{
+		if ((line[i] == '\'' || line[i] == '\"') && quotes_flag == 0)
+		{
+			quote_type = line[i];
+			quotes_flag = 1;
+		}
+		else if (line[i] == quote_type && quotes_flag == 1)
+			quotes_flag = 0;
+		if (line[i + 2] && line[i] == '>' && line[i + 1] == '>' && line[i + 2] == '>' && quotes_flag == 0)
+			break;
+	}
+	return (line[i]);
 }
 
 char check_line_1(const char *line, int i, int quotes_flag, char quote_type)
@@ -42,8 +60,6 @@ char check_line_1(const char *line, int i, int quotes_flag, char quote_type)
 		}
 		else if (line[i] == quote_type && quotes_flag == 1)
 			quotes_flag = 0;
-		if (line[i + 2] && line[i] == '>' && line[i + 1] == '>' && line[i + 2] == '>' && quotes_flag == 0)
-			break ;
 		if (ft_strchr("{}()&`", line[i]) && quotes_flag == 0)
 			break ;
 		if(i > 1 && line[i - 1] != '\\' && line[i] == ';' && quotes_flag == 0
@@ -65,7 +81,6 @@ int syntax_error(const char *line)
 {
 //	printf("SE\n");
 	int i;
-	char is_syntax_error;
 	int quotes_flag;
 	char quote_type;
 	char res;
@@ -73,14 +88,14 @@ int syntax_error(const char *line)
 	quotes_flag = 0;
 	quote_type = 0;
 	i = -1;
-	is_syntax_error = 0;
 	if (next_symbol_after_space(line) == ';' || next_symbol_after_space(line) == '|')
 	{
 		if (next_symbol_after_space(line) == ';')
 			return (with_error(';'));
 		return (with_error('|'));
 	}
-	if ((res = check_line_1(line, i, quotes_flag, quote_type)))
+	if ((res = check_line_1(line, i, quotes_flag, quote_type))
+	|| (res = check_line_2(line, i, quotes_flag, quote_type)))
 		return (with_error(res));
 	return (0);
 }
